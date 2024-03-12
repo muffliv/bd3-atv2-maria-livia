b5715644b29edf5ad556eb57b906089515e76e45
muffliv <livymendes5@gmail.com>
2024-03-12 20:40:14 -0300

    Banco Relacional Escola

---
+++ db3-atv2-inserts.sql
@@ -0,0 +1,45 @@
+INSERT INTO tbl_turma(sigla, nome)
+VALUES ("DS", "DESENVOLVIMENTO DE SISTEMAS");
+
+INSERT INTO tbl_turma(sigla, nome)
+VALUES ("AUTO", "AUTOMAÇÃO INDUSTRIAL");
+
+INSERT INTO tbl_turma(sigla, nome)
+VALUES ("ETIM", " ENSINO MÉDIO INTEGRAL TÉCNICO");
+
+INSERT INTO tbl_turma(sigla, nome)
+VALUES ("ADM", "ADMINISTRAÇÃO");
+
+INSERT INTO tbl_turma(sigla, nome)
+VALUES ("RDC", "REDES DE COMPUTADORES");
+
+##### INSERTS ALUNOS #####
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (2, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (2, 'Alberto', '78358796265', '78358796265', '11996612314', '11996612315', 'alberto@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (2, 'José Bezerra', '56358797245', '86398797243', '12963652310', '12263652317', 'ze@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (2, 'Leandro', '38357196240', '55357196240', '3146660238', '946660234', 'lele@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (2, 'Aurora', '66558796249', '78558796229', '11966602314', '11966602315', 'aurora@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (1, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (1, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (1, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (1, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
+
+INSERT INTO tbl_aluno(cod_turma, nome, cpf, rg, telefone_aluno, telefone_responsavel, email, data_nasc)
+VALUES (1, 'Lisa', '33358796245', '33558696248', '11966602314', '11966602315', 'lisa@gmail.com', 1710286392);
